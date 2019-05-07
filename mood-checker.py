a19ddc6f3f07121e7e2e381c262671fc3b005976
Gea Rak <42340432+GeaFreyja@users.noreply.github.com>
2019-05-07 20:24:22 +0200

    Add files via upload

---
+++ mood-checker.py
@@ -0,0 +1,15 @@
+mood = input("What is your mood? ")
+
+
+if mood == "happy":
+    print("It's gerat to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Don't cry :(")
+elif mood == "excited":
+    print("Juuupiiii!!")
+elif mood == "relaxed":
+    print("Yeeea ;)")
+else:
+    print("I don't recognize this mood")
